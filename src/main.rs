3297bdacdb0514077db7fd7745dc878a7fcd50db
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-31 16:20:13 +0600

    symbolik link

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use symlink::symlink_file;
+
 fn main() {
     println!("Hello, world!");
+    // ? Source File src/main.rs and Link File /home/remon/Documents/main2.rs
+    symlink_file("src/main.rs", "/home/remon/Documents/main2.rs").unwrap();
 }
